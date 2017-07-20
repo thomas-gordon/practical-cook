8be1543be00ad1edefd87e0b71a55f7ef282d964
Tom Gordon <tom.gordon@squareweave.com.au>
2017-07-20 18:21:47 +1000

    Add thanks

---
+++ src/pages/thanks.js
@@ -0,0 +1,25 @@
+import React from "react"
+import get from "lodash/get"
+import Helmet from "react-helmet"
+import PropTypes from 'prop-types';
+
+class ThanksIndex extends React.Component {
+    render() {
+        console.log("props", this.props)
+
+
+        return (
+            <div>
+                <Helmet title={get(this, "props.data.site.siteMetadata.title")} />
+                <h1>Thanks for your submission!</h1>
+                <p>When the site is fully up and running, I'll send you an email with the link.</p>
+            </div>
+        )
+    }
+}
+
+ThanksIndex.propTypes = {
+    route: PropTypes.object,
+};
+
+export default ThanksIndex
